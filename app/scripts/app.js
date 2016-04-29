069758232c5885b4815f10d653d5bb5b1aca4235
Vera Karl <verakarl@c78-104-170-75.fh-salzburg.ac.at>
2016-04-29 11:33:25 +0200

    added app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,14 @@
 import $ from 'jquery'
+import dps from 'dbpedia-sparql-client';
+const query = `SELECT DISTINCT ?StadiumOrArena WHERE {[] a ?StadiumOrArena} LIMIT 10`;
+ 
+dps
+  .client() 
+  .query(query)
+  .timeout(15000) // optional, defaults to 10000
+  .asJson()       // or asXml()
+  .then(r => { /* handle success */})
+  .catch(e => { /* handle error */});
 
-$('h1').html('Hello, World!')
+
+//$('h1').html('Hello, World!')
