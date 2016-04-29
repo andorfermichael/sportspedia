5dd11404c6be6d436b2ae04c0a4cd228cbfae6d9
Stefanie Habersatter <shabersatter.mmt-b2014@fh-salzburg.ac.at>
2016-04-29 13:59:45 +0200

    [BugFix] Fix lil Syntax Error

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,13 @@
 import dps from 'dbpedia-sparql-client';
-const querySportsstadium = `SELECT * WHERE {
+const querySportsstadium = 'SELECT * WHERE {
   ?s a onto:SportFacility .
   ?s geo:lat ?lat .
   ?s geo:long ?long .
  FILTER ( ?long > 13.033229 - 0.1 && ?long < 13.033229 + 0.1 && ?lat > 47.811195 - 0.1 && ?lat < 47.811195 + 0.1 )
-}`;
- 
+}';
+
 dps
-  .client() 
+  .client()
   .query(querySportsstadium)
   .timeout(15000) // optional, defaults to 10000
   .asJson()       // or asXml()
