463770f61c771b200aed35ba1f5502b48774cc10
Vera Karl <verakarl@c78-104-170-75.fh-salzburg.ac.at>
2016-04-29 13:32:01 +0200

    deleted unnecessary things and added a tryoutquery

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,20 @@
 import $ from 'jquery'
 import dps from 'dbpedia-sparql-client';
-const query = `SELECT DISTINCT ?StadiumOrArena WHERE {[] a ?StadiumOrArena} LIMIT 10`;
+const querySportsstadium = `PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> 
+PREFIX onto: <http://dbpedia.org/ontology/> 
+
+SELECT * WHERE {
+  ?s a onto:SportFacility .
+  ?s geo:lat ?lat .
+  ?s geo:long ?long .
+ FILTER ( ?long > 13.033229 - 0.1 && ?long < 13.033229 + 0.1 && ?lat > 47.811195 - 0.1 && ?lat < 47.811195 + 0.1 )
+}`;
  
 dps
   .client() 
-  .query(query)
+  .query(querySportsstadium)
   .timeout(15000) // optional, defaults to 10000
   .asJson()       // or asXml()
-  .then(r => { /* handle success */})
-  .catch(e => { /* handle error */});
+  .then(r => { console.log(querySportsstadium)})
+  .catch(e => { console.log("the query can not be fulfilled")});
 
