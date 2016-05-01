040d77d9ebbfdfe2ce321332b3cc9f3f4e672b20
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-05-01 14:13:31 +0200

    [chore] remove incompatible code

--- app/components/cesium-widget.js
+++ app/components/cesium-widget.js
@@ -1,19 +1,7 @@
 import Ember from 'ember';
 import $ from 'jquery';
-//import dps from 'npm:dbpedia-sparql-client';
-//import Cesium from 'vendor/cesium/Cesium';
-
-/*
-PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-  PREFIX onto: <http://dbpedia.org/ontology/>
-  SELECT * WHERE {
-  ?s a onto:SportFacility .
-    ?s geo:lat ?lat .
-    ?s geo:long ?long .
-    FILTER ( ?long > 13.033229 - 0.1 && ?long < 13.033229 + 0.1 && ?lat > 47.811195 - 0.1 && ?lat < 47.811195 + 0.1 )
-}
-*/
-
+import dp from 'npm:dbpedia-sparql-client';
+const dps = dp.default;
 
 export default Ember.Component.extend({
   didRender() {
@@ -37,17 +25,17 @@ export default Ember.Component.extend({
         longitude: 13.0333333
     };
 
-  function morph(target_projection){
-    if (target_projection === "2D"){
-      scene.morphTo2D();
+    function morph(target_projection){
+      if (target_projection === "2D"){
+        scene.morphTo2D();
+      }
+      else if (target_projection === "3D"){
+        scene.morphTo3D();
+      }
+      else if (target_projection === "Columbus"){
+        scene.morphToColumbusView();
+      }
     }
-    else if (target_projection === "3D"){
-      scene.morphTo3D();
-    }
-    else if (target_projection === "Columbus"){
-      scene.morphToColumbusView();
-    }
-  }
 
   /*
   function spinGlobe(dynamicRate){
@@ -62,195 +50,150 @@ export default Ember.Component.extend({
   }
 */
 
-  function toggleControls(option){
-    scene.screenSpaceCameraController.enableRotate = option;
-    scene.screenSpaceCameraController.enableTranslate = option;
-    scene.screenSpaceCameraController.enableZoom = option;
-    scene.screenSpaceCameraController.enableTilt = option;
-    scene.screenSpaceCameraController.enableLook = option;
-  }
+    function toggleControls(option){
+      scene.screenSpaceCameraController.enableRotate = option;
+      scene.screenSpaceCameraController.enableTranslate = option;
+      scene.screenSpaceCameraController.enableZoom = option;
+      scene.screenSpaceCameraController.enableTilt = option;
+      scene.screenSpaceCameraController.enableLook = option;
+    }
 
-  function hideElements(){
-    $('.cesium-viewer-animationContainer').toggle();
-    $('.cesium-widget-credits').toggle();
-  }
+    function hideElements(){
+      $('.cesium-viewer-animationContainer').toggle();
+      $('.cesium-widget-credits').toggle();
+    }
 
-  function setView(target = currentposition,height = 3939999.0){
-    // to Salzburg
-    //viewer.camera.lookAt(center, new Cesium.Cartesian3(0.0, -4790000.0, 3930000.0));
-    //var center = Cesium.Cartesian3.fromDegrees(13.0333333, 47.8, 3930000.0);
-    var center = Cesium.Cartesian3.fromDegrees(target.longitude, target.latitude, height);
-    viewer.camera.flyTo({
-      destination : center
-    });
-  }
-
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position){
-        currentposition = {
-          longitude: position.coords.longitude,
-          latitude: position.coords.latitude
-        };
+    function setView(target = currentposition,height = 3939999.0){
+      // to Salzburg
+      //viewer.camera.lookAt(center, new Cesium.Cartesian3(0.0, -4790000.0, 3930000.0));
+      //var center = Cesium.Cartesian3.fromDegrees(13.0333333, 47.8, 3930000.0);
+      var center = Cesium.Cartesian3.fromDegrees(target.longitude, target.latitude, height);
+      viewer.camera.flyTo({
+        destination : center
       });
     }
-  }
 
-  //spinGlobe(0.01);
-  setTimeout(function(){
-    getLocation();
-    setView();
-  },3000);
-  toggleControls(false);
-  hideElements();
-
-  /*$(function() {
-    $( "#draggable" ).draggable();
-  });
-*/
-  $('#start-button').click(function(){
-    morph("2D");
-    $('main').toggle();
-    toggleControls(true);
-    setTimeout(function(){
-      //setView(currentposition,50000);
-      //getData();
-      getDataNew();
-      var entityArray = [];
-      entityArray.push({
-        name: "Red Bull Arena Salzburg",
-        description: "Die Red Bull Arena ist ein österreichisches Fußballstadion am Stadtrand von Salzburg, in der Gemeinde Wals-Siezenheim. Es ist Heimstadion des Bundesligisten FC Red Bull Salzburg sowie des Erstligisten FC Liefering und fasst insgesamt 30.180[2] Zuschauer. Bis nach der Fußball-Europameisterschaft 2008, bei der das Stadion eines der vier Austragungsorte in Österreich war, hieß es EM-Stadion Wals-Siezenheim.",
-        coords: {
-          latitude: 47.8163445,
-          longitude: 12.9981943
-        }
-      });
-      entityArray.push({
-        name: "Eisarena Salzburg",
-        description: "Das ist Mike's Lieblingsarena",
-        coords: {
-          latitude: 47.797731,
-          longitude: 13.059888
-        }
-      });
-
-      zoomToPins(
-        createMultiplePins(entityArray)
-      );
-
-      //toggleInfoBox();
-    },4000);
-
-  });
-
-  //PINS
-  function createSinglePin(entity,pin_img = 'Assets/Textures/maki/marker-stroked.png'){
-    /*
-    entity Element expected in JSON with following structure
-     var entity = {
-       name: "this is the stadium name",
-       desctiption: "this is the stadium description",
-       otherattributes: "and much more",
-       image: "external image url",
-       coords: {
-         longitude: 12.9922660309,
-         latitude: 47.8097550943
-       }
-     }
-    }
-     */
-    var url = Cesium.buildModuleUrl(pin_img);
-    var Pin = Cesium.when(pinBuilder.fromUrl(url, Cesium.Color.WHITE, 48), function(canvas) {
-      return viewer.entities.add({
-        name : entity.name,
-        description : entity.description,
-        //47.8097550943 12.9922660309
-        position : Cesium.Cartesian3.fromDegrees(entity.coords.longitude,entity.coords.latitude),
-        billboard : {
-          image : canvas.toDataURL(),
-          verticalOrigin : Cesium.VerticalOrigin.BOTTOM
-        },
-        point: {
-          pixelSize: 5,
-          color:  Cesium.Color.RED,
-          outlineColor: Cesium.Color.WHITE,
-          outlineWidth: 2
-        },
-        label:  {
-          text : entity.name,
-          font : '14pt monospace',
-          style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-          outlineWidth : 2,
-          verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
-          pixelOffset : new Cesium.Cartesian2(0, 20)
-        }
-      });
-    });
-    return Pin;
-  }
-
-  function createMultiplePins(entityArray){
-    //See createSinglePin function for reference of the structure of entity
-    var PinCollection = [];
-    for (var i = 0; i < entityArray.length; i++) {
-        PinCollection.push(createSinglePin(entityArray[i]));
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+          currentposition = {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude
+          };
+        });
       }
-    return PinCollection;
-  }
+    }
 
-  function zoomToPins(pinsarray){
-    //Since some of the pins are created asynchronously, wait for them all to load before zooming/
-    Cesium.when.all(pinsarray, function(pins){
-      viewer.flyTo(pins);
+    //spinGlobe(0.01);
+    setTimeout(function(){
+      getLocation();
+      setView();
+    },3000);
+    toggleControls(false);
+    hideElements();
+
+    /*$(function() {
+      $( "#draggable" ).draggable();
     });
+    */
+    $('#start-button').click(function(){
+      morph("2D");
+      $('main').toggle();
+      toggleControls(true);
+      setTimeout(function(){
+        setView(currentposition,50000);
+        getData();
+        var entityArray = [];
+        entityArray.push({
+          name: "Red Bull Arena Salzburg",
+          description: "Die Red Bull Arena ist ein österreichisches Fußballstadion am Stadtrand von Salzburg, in der Gemeinde Wals-Siezenheim. Es ist Heimstadion des Bundesligisten FC Red Bull Salzburg sowie des Erstligisten FC Liefering und fasst insgesamt 30.180[2] Zuschauer. Bis nach der Fußball-Europameisterschaft 2008, bei der das Stadion eines der vier Austragungsorte in Österreich war, hieß es EM-Stadion Wals-Siezenheim.",
+          coords: {
+            latitude: 47.8163445,
+            longitude: 12.9981943
+          }
+        });
+        entityArray.push({
+          name: "Eisarena Salzburg",
+          description: "Das ist Mike's Lieblingsarena",
+          coords: {
+            latitude: 47.797731,
+            longitude: 13.059888
+          }
+        });
+
+        zoomToPins(
+          createMultiplePins(entityArray)
+        );
+
+        //toggleInfoBox();
+      },4000);
+
+    });
+
+    //PINS
+    function createSinglePin(entity,pin_img = 'Assets/Textures/maki/marker-stroked.png'){
+      /*
+      entity Element expected in JSON with following structure
+       var entity = {
+         name: "this is the stadium name",
+         desctiption: "this is the stadium description",
+         otherattributes: "and much more",
+         image: "external image url",
+         coords: {
+           longitude: 12.9922660309,
+           latitude: 47.8097550943
+         }
+       }
+      }
+       */
+      var url = Cesium.buildModuleUrl(pin_img);
+      var Pin = Cesium.when(pinBuilder.fromUrl(url, Cesium.Color.WHITE, 48), function(canvas) {
+        return viewer.entities.add({
+          name : entity.name,
+          description : entity.description,
+          //47.8097550943 12.9922660309
+          position : Cesium.Cartesian3.fromDegrees(entity.coords.longitude,entity.coords.latitude),
+          billboard : {
+            image : canvas.toDataURL(),
+            verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+          },
+          point: {
+            pixelSize: 5,
+            color:  Cesium.Color.RED,
+            outlineColor: Cesium.Color.WHITE,
+            outlineWidth: 2
+          },
+          label:  {
+            text : entity.name,
+            font : '14pt monospace',
+            style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+            outlineWidth : 2,
+            verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
+            pixelOffset : new Cesium.Cartesian2(0, 20)
+          }
+        });
+      });
+      return Pin;
+    }
+
+    function createMultiplePins(entityArray){
+      //See createSinglePin function for reference of the structure of entity
+      var PinCollection = [];
+      for (var i = 0; i < entityArray.length; i++) {
+          PinCollection.push(createSinglePin(entityArray[i]));
+        }
+      return PinCollection;
+    }
+
+    function zoomToPins(pinsarray){
+      //Since some of the pins are created asynchronously, wait for them all to load before zooming
+      Cesium.when.all(pinsarray, function(pins){
+        viewer.flyTo(pins);
+      });
+    }
+
+    function getData(){
+   
+    }
   }
-
-/*
-  function getData() {
-    const querySportsstadium = 'PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>'+
-      'PREFIX onto: <http://dbpedia.org/ontology/>' +
-      'SELECT * WHERE {' +
-      '?s a onto:SportFacility .' +
-      '?s geo:lat ?lat .' +
-      '?s geo:long ?long .' +
-      'FILTER ( ?long > 13.033229 - 0.1 && ?long < 13.033229 + 0.1 && ?lat > 47.811195 - 0.1 && ?lat < 47.811195 + 0.1 )}';
-
-   new SparqlClient
-      .client()
-      .query(querySportsstadium)
-      .timeout(15000) // optional, defaults to 10000
-      .asJson()       // or asXml()
-      .then(r => { console.log(querySportsstadium)})
-      .catch(e => { console.log("the query can not be fulfilled")})
-  }*/
-
-  //neue schreibweise wg. neuem sparql-client
-  //https://www.npmjs.com/package/sparql-client
-  function getDataNew(){
-  	var SparqlClient = require('sparql-client');
-	var util = require('util');
-	var endpoint = 'http://dbpedia.org/sparql';
-	var query = 'PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>'+
-      'PREFIX onto: <http://dbpedia.org/ontology/>' +
-      'SELECT * WHERE {' +
-      '?s a onto:SportFacility .' +
-      '?s geo:lat ?lat .' +
-      '?s geo:long ?long .' +
-      'FILTER ( ?long > 13.033229 - 0.1 && ?long < 13.033229 + 0.1 && ?lat > 47.811195 - 0.1 && ?lat < 47.811195 + 0.1 )}';
-
-	var client = new SparqlClient(endpoint);
-	console.log("Query to " + endpoint);
-	console.log("Query: " + query);
-	client.query(query)
-	  //.bind('city', 'db:Chicago') 
-	  //.bind('city', 'db:Tokyo') 
-	  //.bind('city', 'db:Casablanca') 
-	  .bind('SportFacility', '<http://dbpedia.org/ontology/>')
-	  .asJson()
-	  .execute(function(error, results) {
-	  process.stdout.write(util.inspect(arguments, null, 20, true)+"\n");1
-	});
- 
-  }
-}
 });
